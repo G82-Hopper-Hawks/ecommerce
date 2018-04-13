14d5ee0bcd4a366a60610bd2ca2f4e78c86dbdb2
Sunil Kakar <26909769+DrPsyFi@users.noreply.github.com>
2018-04-13 09:22:52 -0700

    feature completed

--- checkout.js
+++ checkout.js
@@ -23,3 +23,43 @@ if(check.checked === true) {
     document.querySelector('.billZip').value = '';
   }
 }
+
+
+
+
+let unHide = document.querySelector('.hidden')
+let completePurchase = document.querySelector('#purchase')
+
+completePurchase.addEventListener('click', function() {
+  let billFirst = document.querySelector('.billFname')
+  let billLast = document.querySelector('.billLname')
+  let billAdd1 = document.querySelector('.billAdd1')
+  let billCity = document.querySelector('.billCity')
+  let billState = document.querySelector('.billState')
+  let billZip = document.querySelector('.billZip')
+  let shipFirst = document.querySelector('.shipFname')
+  let shipLast = document.querySelector('.shipLname')
+  let shipAdd1 = document.querySelector('.shipAdd1')
+  let shipCity = document.querySelector('.shipCity')
+  let shipState = document.querySelector('.shipState')
+  let shipZip = document.querySelector('.shipZip')
+  let cCard = document.querySelector('.cCard')
+  let ccV = document.querySelector('.ccV')
+
+
+
+  let ship_bill_check = [billFirst, billLast, billAdd1, billCity, billState, billZip, shipFirst, shipLast, shipAdd1, shipCity, shipState, shipZip, ccV, cCard]
+  fillCheck(ship_bill_check)
+})
+
+function fillCheck(arr) {
+  let target = event.target
+
+  arr.forEach(function(element) {
+    console.log(element);
+    if (element && !element.value) {
+        unHide.classList.remove('hidden')
+        element.classList.toggle('required')
+      }
+  })
+}
